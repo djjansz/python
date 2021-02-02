5329683b8e487395faddfccaa1f5da86e47ae9f7
djjansz <djjansz@gmail.com>
2021-02-01 22:49:09 -0500

    Expand on the comments.
    Add additional comments.

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,7 @@
-# Generate psuedorandom numbers
 from random import seed
 from random import random
 from math import floor
+# use the seed to initiate a psuedorandom number generator
 input_seed=input("Enter seed: ")
 secret_num=floor(random()*100)
 guess = 0
@@ -9,6 +9,7 @@ guess_count=0
 guess_limit=5
 out_of_guesses = False
 
+# while the user has not guessed it correctly, iterate and prompt for a new guess
 while guess != secret_num and not(out_of_guesses):
     if guess_count < guess_limit:
          guess = input("Enter guess (1 to 100): ")
